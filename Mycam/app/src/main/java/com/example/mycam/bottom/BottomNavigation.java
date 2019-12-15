e4d2da7b48e6d7820c4e919e4b101a12d7627117
Mochamadwildan <50358445+Mochamadwildan@users.noreply.github.com>
2019-12-15 22:15:26 +0700

    add api kamera dan get kamera

--- Mycam/app/src/main/java/com/example/mycam/bottom/BottomNavigation.java
+++ Mycam/app/src/main/java/com/example/mycam/bottom/BottomNavigation.java
@@ -1,29 +1,57 @@
 package com.example.mycam.bottom;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.mycam.R;
+import com.example.mycam.bottom.ui.admin.AdminFragment;
+import com.example.mycam.bottom.ui.kategori.KategoriFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
 public class BottomNavigation extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bottom_navigation);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                 R.id.navigation_kategori, R.id.navigation_admin)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+        loadFragment(new KategoriFragment());
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment = null;
+                switch (menuItem.getItemId()) {
+                    case R.id.navigation_kategori:
+                        fragment = new KategoriFragment();
+                        break;
+                    case R.id.navigation_admin:
+                        fragment = new AdminFragment();
+                        break;
+                }
+                return loadFragment(fragment);
+            }
+        });
     }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .addToBackStack(null)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+
 
 }
